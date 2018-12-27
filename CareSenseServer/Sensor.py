664f158f89f422f16f1097cff52bb552a84c48d2
Philippe Lorange <philippe.lorange@gmail.com>
2018-12-27 16:43:42 -0500

    removing CareSenseServer

--- CareSenseServer/Sensor.py
+++
@@ -1,9 +0,0 @@
-class Sensor:
-    id = 0
-    roomOne = ""
-    roomTwo = ""
-
-    def __init__(self, sensor_id, room_one, room_two):
-        self.id = sensor_id
-        self.roomOne = room_one
-        self.roomTwo = room_two
